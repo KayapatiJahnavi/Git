10ba797745fa0c32b4f8c65017f79fcfa72acac1
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-07-28 09:48:16 +0530

    Create Git6.java

---
+++ Git6.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Function to compute GCD of two numbers
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public int countCoprimePairs(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (gcd(arr[i], arr[j]) == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {2, 3, 4, 5};
+        System.out.println(sol.countCoprimePairs(arr)); // Output: 5
+    }
+}
