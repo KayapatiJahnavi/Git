9f7a6cadb91dba8e18ececd23e636c54a65788f6
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-07-29 08:44:33 +0530

    Create git7.java

---
+++ git7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+
+    public static void add_to_ArrayList(ArrayList<Integer> A, int x) {
+        
+        
+        A.add(x);
+
+       
+    }
+
+    public static void sort_ArrayList_Asc(ArrayList<Integer> A) {
+
+        
+        
+        
+        Collections.sort(A);
+        
+    }
+
+        
+    public static void reverse_ArrayList(ArrayList<Integer> A) {
+        
+        Collections.reverse(A);
+        
+    }
+        
+
+    public static int size_Of_ArrayList(ArrayList<Integer> A) {
+        
+        return (A.size());
+        
+    }
