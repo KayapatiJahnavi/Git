494d1d1d5714f03222367bf519ef3e7e1b477293
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-07-30 09:50:43 +0530

    Create Git11.java

---
+++ Git11.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int windowSize = wordLen * wordCount;
+
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String w : words) {
+            wordMap.put(w, wordMap.getOrDefault(w, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {  
+            int left = i;
+            int right = i;
+            int count = 0;
+            Map<String, Integer> seen = new HashMap<>();
+
+            while (right + wordLen <= s.length()) {
+                String word = s.substring(right, right + wordLen);
+                right += wordLen;
+
+                if (wordMap.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (seen.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        count--;
+                        left += wordLen;
+                    }
+
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    seen.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+        return result;
+    }
+}
+
