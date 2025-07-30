d5b405d5363bf43951e149166601818d5e6a7bd0
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-07-30 09:49:33 +0530

    Create Git10.java

---
+++ Git10.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+
+            // Swap
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+
+            // Move to the next pair
+            current = first;
+        }
+
+        return dummy.next;
+    }
+}
