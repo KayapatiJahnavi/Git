8fc94ce66118f42489d09357242a72d08c6663c2
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-08-08 19:11:57 +0530

    Update Zero Array Transformation II.java

--- Zero Array Transformation II.java
+++ Zero Array Transformation II.java
@@ -1,27 +1,30 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-
-        for (int k = 0; k < queries.length; k++) {
-            int li = queries[k][0];
-            int ri = queries[k][1];
-            int val = queries[k][2];
-
-            for (int i = li; i <= ri; i++) {
-                nums[i] = Math.max(0, nums[i] - val);
-            }
-
-            if (allZero(nums)) {
-                return k + 1; // because k is 0-indexed
-            }
+        
+        if (Arrays.stream(nums).allMatch(x -> x == 0)) return 0;
+        int left = 1, right = queries.length;
+        if (!canMakeZeroArray(right, nums, queries)) return -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canMakeZeroArray(mid, nums, queries)) right = mid;
+            else left = mid + 1;
         }
-
-        return -1;
+        return left;
     }
 
-    private boolean allZero(int[] nums) {
-        for (int num : nums) {
-            if (num != 0) return false;
+    private boolean canMakeZeroArray(int k, int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+        for (int i = 0; i < k; i++) {
+            int left = queries[i][0], right = queries[i][1], val = queries[i][2];
+            diff[left] += val;
+            diff[right + 1] -= val;
+        }
+        int currVal = 0;
+        for (int i = 0; i < n; i++) {
+            currVal += diff[i];
+            if (currVal < nums[i]) return false;
         }
         return true;
     }
